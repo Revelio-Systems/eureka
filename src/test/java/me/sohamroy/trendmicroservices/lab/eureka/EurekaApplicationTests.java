31b427f415c641abf482341852a4011672582535
Soham Roy <sohamroy@sohamroy.me>
2020-05-09 16:22:59 -0500

    Init commit

---
+++ src/test/java/me/sohamroy/trendmicroservices/lab/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sohamroy.trendmicroservices.lab.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
